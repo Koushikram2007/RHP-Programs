4faaa6391891a73cb4ee8a55bcb2a3fdcb68578e
Koushikram2007 <25cd034@rmkec.ac.in>
2026-06-25 15:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (69.20%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,15 +1,13 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
+class Solution{
+    public int lengthOfLastWord(String s){
+        int i=s.length()- 1;
+        while (i>=0 && s.charAt(i)==' '){
+            i--;
+        }
         int count=0;
-        String str=s.trim();
-        String Str=new StringBuilder(str).reverse().toString();
-        for (char c:Str.toCharArray()){
-            if (c==' '){
-                return count;
-            }
-            else{
-                count++;
-            }
+        while (i>=0 && s.charAt(i) !=' '){
+            count++;
+            i--;
         }
         return count;
     }
