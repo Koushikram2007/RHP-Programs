7005366bfebde86869b8dd1b350ebb2bb289e8e0
Koushikram2007 <25cd034@rmkec.ac.in>
2026-08-24 19:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (71.16%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int find(vector<int>& ldr,int node){
+        if (ldr[node]==node){
+            return node;
+        }
+        return ldr[node]=find(ldr,ldr[node]);
+    }
+    void union_set(vector<int>& ldr,int lt,int rt){
+        ldr[find(ldr,lt)]=find(ldr,rt);
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int N=isConnected.size();
+        vector<int> ldr(N,0);
+        for (int i=1;i<N;i++){
+            ldr[i]=i;
+        }
+        for (int r=0;r<N;r++){
+            for (int c=r+1;c<N;c++){
+                if (isConnected[r][c]==1){
+                    union_set(ldr,r,c);
+                }
+            }
+        }
+        int count=0;
+        for (int node=0;node<N;node++){
+            if (ldr[node]==node){
+                count++;
+            }
+        }
+        return count;
+    }
+};
